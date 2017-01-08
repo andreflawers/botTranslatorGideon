a6ec2b19d46251a6b3156cb68f6fcb7e878cebc5
andreflawers <andreflawers@outlook.com>
2017-01-08 15:04:42 -0500

    added a lot

--- app.js
+++ app.js
@@ -1,25 +1,180 @@
 var builder = require('botbuilder');
 var restify = require('restify');
-var botConnectorOptions = { 
+var request = require('request');
+var parseString = require('xml2js').parseString;
 
-appId: process.env.BOTFRAMEWORK_APPID, 
-    appPassword: process.env.BOTFRAMEWORK_APPSECRET 
- };
+var botConnectorOptions = {
 
+    appId: '324f8eae-2b3b-4ee9-a767-f0e75a9c19ea',
+    appPassword: 'BRax17KiinxpiAZgfKJkK73'
+};
+var textoTraducir = '';
 var server = restify.createServer();
-server.listen(process.env.port||process.env.PORT||3978,function () { 
-    console.log('%s listening to %s',server.name,server.url);
- });
+var language;
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
 
 var connector = new builder.ChatConnector(botConnectorOptions);
 var bot = new builder.UniversalBot(connector);
-server.get('/.*/',restify.serveStatic({
-    'directory':'.',
-    'default':'index.html'
+server.get('/.*/', restify.serveStatic({
+    'directory': '.',
+    'default': 'index.html'
 }));
-server.post('/api/messages',connector.listen());
-bot.dialog('/',[function (session) {  
-    builder.Prompts.text(session,'Hola soy Gideon un gato que traduce (tambien puedo escribir, no te sorprendas) como desees que te llame?');
-},function (session,results) {  
-    session.send('Hola ' + results.response);    
+server.post('/api/messages', connector.listen());
+
+bot.dialog('/', [function (session) {
+    builder.Prompts.choice(session, 'Estas son las opciones , cual eligess :3 ?', 'texto|imagen');
+
+}, function (session, result) {
+    if (result.response.entity == 'imagen') {
+        session.beginDialog('/translateImage');
+    }
+    else if (result.response.entity == 'texto') {
+        session.beginDialog('/translateText');
+    }
 }]);
+bot.dialog('/translateImage', [
+    function (session) {
+        builder.Prompts.attachment(session, 'Sube tu imagen :D ');
+    },
+    function (session, result) {
+        var imageUrl = result.response[0].contentUrl;
+
+        var postOptions = {
+            headers: {
+                "Ocp-Apim-Subscription-Key": "585f715a968340f89cb2455c38fe5c0e"
+            },
+            url: "http://api.projectoxford.ai/vision/v1.0/ocr?language=unk&detectOrientation=true",
+            encoding: 'binary',
+        };
+        request.get(imageUrl).pipe(request.post(postOptions, function (error, response, body) {
+            console.log('execute post');
+            if (error) {
+                console.log(error);
+            }
+            var resultOCR = JSON.parse(body);
+            language = resultOCR.language;
+            console.log('text language:  %s', resultOCR.language);
+            for (var region in resultOCR.regions) {
+                for (var line in resultOCR.regions[region].lines) {
+                    for (var word in resultOCR.regions[region].lines[line].words) {
+
+                        textoTraducir += resultOCR.regions[region].lines[line].words[word].text + " ";
+
+
+                    }
+                }
+            }
+            session.send('Texto a traducir : ');
+            session.send(textoTraducir);
+            translating(function (error, textTranslated) {
+                if (error)
+                { console.log(error); }
+                else {
+                    console.log(textTranslated);
+                    session.endDialog(textTranslated);
+                    textoTraducir = '';
+                }
+            });
+        }));
+
+
+        textoTraducir = '';
+
+    }]);
+
+bot.dialog('/translateText', [function (session) {
+    builder.Prompts.text(session, 'Ingresa la palabra o texto a traducir :3 ');
+}, function (session, result) {
+
+    textoTraducir = result.response;
+    request.post(
+        {
+            headers: {
+                'Ocp-Apim-Subscription-Key': '83221201a8634cb0a5498edc1799cf97',
+                'Content-Type': 'application/json'
+            },
+            url: 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/languages',
+            body: '\{"documents": \[\{"id": "textoTraducir", "text": "'+textoTraducir+'"}]}' 
+        }, function (error, response, body) {
+    if (error)
+    { console.error(error) }
+    var bodyJson = JSON.parse(body);
+    console.log('lenguaje :'+ bodyJson.documents[0].detectedLanguages[0].iso6391Name);
+    textoTraducir = '';
+}
+
+);
+
+
+
+}]);
+
+bot.dialog('/firstRun', [
+    function (session) {
+        session.userData.version = 1.0;
+        session.send('Hola %s soy Gideon un gato traductor (tambien puedo escribir, no te sorprendas)', session.message.user.name);
+        var msg = new builder.Message(session)
+            .attachments([{
+                contentType: 'image/gif',
+                contentUrl: 'http://media3.giphy.com/media/13HBDT4QSTpveU/200.gif'
+            }]);
+        session.endDialog(msg);
+
+    }
+
+]).triggerAction({
+    onFindAction: function (context, callback) {
+
+        var ver = context.userData.version || 0;
+        var score = ver < 1.0 ? 1.1 : 0.0;
+        callback(null, score);
+    }
+});
+
+const translating = function (callback) {
+    var accessToken;
+    request.post({
+        url: 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': ' application/jwt',
+            'Ocp-Apim-Subscription-Key': '4f8df7892de74d76a662d9a2166ce4f6'
+
+        }
+    }, function (error, response, body) {
+        if (error) {
+            callback(error, null);
+            return;
+        }
+        accessToken = "Bearer " + body;
+
+        var languageTo = 'es';
+        if (language == 'es')
+        { languageTo = 'en' };
+
+        request.get('https://api.microsofttranslator.com/v2/http.svc/Translate?appid=' + accessToken +
+            '&text=' + textoTraducir +
+            '&from=' + language +
+            '&to=' + languageTo, function (error, response, body) {
+
+                if (error) {
+                    console.error(error);
+                    callback(error, null);
+                }
+
+                parseString(body, function (err, result) {
+
+                    var textoTraducido = JSON.parse(JSON.stringify(result));
+                    console.dir('texto traducido ' + textoTraducido.string._);
+
+                    callback(null, textoTraducido.string._);
+
+                });
+
+            });
+
+
+    });
+}
